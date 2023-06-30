327c33f9799e51e83ca1fcad5bf8e826225046d2
Sanjit Juneja <sanjit.j55@gmail.com>
2023-06-29 21:51:05 -0500

    More changes

--- app.py
+++ app.py
@@ -1,33 +1,150 @@
-from langchain import OpenAI, LLMMathChain, SerpAPIWrapper
-from langchain.agents import initialize_agent, Tool
-from langchain.chat_models import ChatOpenAI
+import openai
+import json
+import ast
 import os
 import chainlit as cl
 
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["SERPAPI_API_KEY"] = "fc35ee3159ee64b6f23fa05b2083b87e6a6ccd9178f961b2e4196ce5f7b510ae"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+MAX_ITER = 5
+
+# Example dummy function hard coded to return the same weather
+# In production, this could be your backend API or an external API
+def get_current_weather(location, unit):
+    """Get the current weather in a given location"""
+    unit = unit or "Farenheit"
+    weather_info = {
+        "location": location,
+        "temperature": "60",
+        "unit": unit,
+        "forecast": ["windy"],
+    }
+
+    return json.dumps(weather_info)
 
 
-# The search tool has no async implementation, we fall back to sync
-@cl.langchain_factory(use_async=False)
-def load():
-    llm = ChatOpenAI(temperature=0, streaming=True)
-    llm1 = OpenAI(temperature=0, streaming=True)
-    search = SerpAPIWrapper()
-    llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+functions = [
+    {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+            },
+            "required": ["location"],
+        },
+    }
+]
 
-    tools = [
-        Tool(
-            name="Search",
-            func=search.run,
-            description="useful for when you need to answer questions about current events. You should ask targeted questions",
-        ),
-        Tool(
-            name="Calculator",
-            func=llm_math_chain.run,
-            description="useful for when you need to answer questions about math",
-        ),
-    ]
-    return initialize_agent(
-        tools, llm1, agent="chat-zero-shot-react-description", verbose=True
+
+async def process_new_delta(new_delta, openai_message, content_ui_message, function_ui_message):
+    if "role" in new_delta:
+        openai_message["role"] = new_delta["role"]
+    if "content" in new_delta:
+        new_content = new_delta.get("content") or ""
+        openai_message["content"] += new_content
+        await content_ui_message.stream_token(new_content)
+    if "function_call" in new_delta:
+        if "name" in new_delta["function_call"]:
+            openai_message["function_call"] = {
+                "name": new_delta["function_call"]["name"]}
+            await content_ui_message.send()
+            function_ui_message = cl.Message(
+                author=new_delta["function_call"]["name"],
+                content="", indent=1, language="json")
+            await function_ui_message.stream_token(new_delta["function_call"]["name"])
+
+        if "arguments" in new_delta["function_call"]:
+            if "arguments" not in openai_message["function_call"]:
+                openai_message["function_call"]["arguments"] = ""
+            openai_message["function_call"]["arguments"] += new_delta["function_call"]["arguments"]
+            await function_ui_message.stream_token(new_delta["function_call"]["arguments"])
+    return openai_message, content_ui_message, function_ui_message
+
+
+@cl.on_chat_start
+async def start_chat():
+    cl.user_session.set(
+        "message_history",
+        [{"role": "system", "content": "You are Natural Law, an AI assistant with the knowledge of all legal proceedings, court cases, and other legal info to give proper legal advice. You are also equipped with moral capabilities trained on the greatest philosophers and philosophical texts of our time."}],
     )
+    res = await cl.AskUserMessage(content="What is your name?", timeout=30).send()
+    if res:
+        await cl.Message(
+            content=f"Your name is: {res['content']}.\nChainlit installation is working!\nYou can now start building your own chainlit apps!",
+        ).send()
+
+
+@cl.on_message
+async def run_conversation(user_message: str):
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"role": "user", "content": user_message})
+
+    cur_iter = 0
+
+    while cur_iter < MAX_ITER:
+
+        # OpenAI call
+        openai_message = {"role": "", "content": ""}
+        function_ui_message = None
+        await cl.Avatar(
+            name="Natural Law",
+            path="assets/avatar.png"
+        ).send()
+        content_ui_message = cl.Message(content="")
+        async for stream_resp in await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo-0613",
+            messages=message_history,
+            stream=True,
+            function_call="auto",
+            functions=functions,
+            temperature=0
+        ):
+
+            new_delta = stream_resp.choices[0]["delta"]
+            openai_message, content_ui_message, function_ui_message = await process_new_delta(
+                new_delta, openai_message, content_ui_message, function_ui_message)
+
+        message_history.append(openai_message)
+        if function_ui_message is not None:
+            await function_ui_message.send()
+
+        if stream_resp.choices[0]["finish_reason"] == "stop":
+            break
+
+        elif stream_resp.choices[0]["finish_reason"] != "function_call":
+            raise ValueError(stream_resp.choices[0]["finish_reason"])
+
+        # if code arrives here, it means there is a function call
+        function_name = openai_message.get("function_call").get("name")
+        arguments = ast.literal_eval(
+            openai_message.get("function_call").get("arguments"))
+
+        function_response = get_current_weather(
+            location=arguments.get("location"),
+            unit=arguments.get("unit"),
+        )
+
+        message_history.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+        )
+        await cl.Avatar(
+            name=function_name,
+            path="./assets/avatar.png"
+        ).send()
+        await cl.Message(
+            author=function_name,
+            content=str(function_response),
+            language="json",
+            indent=1,
+        ).send()
+        cur_iter += 1

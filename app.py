d272cd0ddb1a416d420c109c9416ca4bd3b04e44
Sanjit Juneja <sanjit.j55@gmail.com>
2023-07-01 16:12:16 -0500

    Added pdf q&a

--- app.py
+++ app.py
@@ -5,37 +5,145 @@ import chainlit as cl
 from langchain import SerpAPIWrapper
 from langchain.tools import format_tool_to_openai_function
 from langchain.utilities import WikipediaAPIWrapper
-from langchain.agents import Tool, load_tools
+from langchain.agents import Tool
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 search = SerpAPIWrapper()
 wiki = WikipediaAPIWrapper()
+global process_doc_question_chain
 
 MAX_ITER = 5
 
+
+
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+system_template = """Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+ALWAYS return a "SOURCES" part in your answer.
+The "SOURCES" part should be a reference to the source of the document from which you got your answer.
+
+Example of your response should be:
+
+```
+The answer is foo
+SOURCES: xyz
+```
+
+Begin!
+----------------
+{summaries}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+prompt = ChatPromptTemplate.from_messages(messages)
+chain_type_kwargs = {"prompt": prompt}
+
+
 async def legal_doc_upload():
     files = None
+
+    # Wait for the user to upload a file
     while files == None:
         files = await cl.AskFileMessage(
-            content="Please upload a legal doc to begin! (Text File)", accept={"text/plain": [".rtf", ".txt"], "pdf": [".pdf"], "docx": [".docx"]}
+            content="Please upload a text file to begin!", accept=["text/plain"]
         ).send()
+
+    file = files[0]
+
+    msg = cl.Message(content=f"Processing `{file.name}`...")
+    await msg.send()
+
     # Decode the file
-    text_file = files[0]
-    text = text_file.content.decode("utf-8")
+    text = file.content.decode("utf-8")
 
-    await cl.Message(
-        content=f"`{text_file.name}` successfully uploaded, feel free to ask me anything about it!"
-    ).send()
+    # Split the text into chunks
+    texts = text_splitter.split_text(text)
 
-    return text
+    # Create a metadata for each chunk
+    metadatas = [{"source": f"{i}-pl"} for i in range(len(texts))]
 
-generic_tools = load_tools(["serpapi", "wikipedia"])
+    # Create a Chroma vector store
+    embeddings = OpenAIEmbeddings()
+    docsearch = await cl.make_async(Chroma.from_texts)(
+        texts, embeddings, metadatas=metadatas
+    )
+    # Create a chain that uses the Chroma vector store
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        ChatOpenAI(temperature=0),
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+    )
+
+    # Save the metadata and texts in the user session
+    cl.user_session.set("metadatas", metadatas)
+    cl.user_session.set("texts", texts)
+
+    # Let the user know that the system is ready
+    await msg.update(content=f"`{file.name}` processed. You can now ask questions!")
+
+
+    return chain
+
+
+async def process_doc_question(user_prompt: str):
+    global process_doc_question_chain
+    if process_doc_question_chain is None:
+        return "Please upload a document first!"
+    # Get the answer from the chain
+    res = process_doc_question_chain({"question": user_prompt}, return_only_outputs=True)
+
+    answer = res["answer"]
+    sources = res["sources"].strip()
+    source_elements = []
+
+    # Get the metadata and texts from the user session
+    metadatas = cl.user_session.get("metadatas")
+    all_sources = [m["source"] for m in metadatas]
+    texts = cl.user_session.get("texts")
+
+    if sources:
+        found_sources = []
+
+        # Add the sources to the message
+        for source in sources.split(","):
+            source_name = source.strip().replace(".", "")
+            # Get the index of the source
+            try:
+                index = all_sources.index(source_name)
+            except ValueError:
+                continue
+            text = texts[index]
+            found_sources.append(source_name)
+            # Create the text element referenced in the message
+            source_elements.append(cl.Text(content=text, name=source_name))
+
+        if found_sources:
+            answer += f"\nSources: {', '.join(found_sources)}"
+        else:
+            answer += "\nNo sources found"
+
+    # await cl.Message(content=answer, elements=source_elements).send()
+
+    return answer
 
 tools = [
-    generic_tools[0],
-    generic_tools[1],
     Tool(
         name="Search",
         func=search.run,
@@ -50,6 +158,11 @@ tools = [
         name="legal_doc_upload",
         func=legal_doc_upload,
         description="Useful when the user wants to upload a legal document to ask questions or ask for your review. You are a qualified legal consultant able to give legal advice."
+    ),
+    Tool(
+        name="process_doc_question",
+        func=process_doc_question,
+        description="Useful when the user wants to ask questions or ask for your review on a document they have already uploaded with you. You are a qualified legal consultant able to give legal advice. Simply pass in the user prompt as a string as the only argument (prompt: str)."
     )
 ]
 
@@ -145,12 +258,16 @@ async def run_conversation(user_message: str):
                     query=arguments.get("query"),
                 )
             case "legal_doc_upload":
-                function_response = await legal_doc_upload()
+                global process_doc_question_chain
+                process_doc_question_chain = await legal_doc_upload()
+                if process_doc_question_chain:
+                    function_response = "Document uploaded successfully!"
+            case "process_doc_question":
+                if arguments.get("prompt") is None:
+                    arguments["prompt"] = arguments.get("__arg1")
+                function_response = await process_doc_question(arguments.get("prompt"))
             case _:
                 raise ValueError(f"Unknown function name: {function_name}")
-            
-        if len(function_response) > 1000:
-            function_response = function_response[:1000]
 
         message_history.append(
             {
